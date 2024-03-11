11d2af7dd396f4de5b8034dd6bb18b42a887276d
Divya <chdivya569@gmail.com>
2024-03-11 18:07:18 +0530

    other test cases

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -9,4 +9,45 @@ class StringCalculator
   def contains_a_single_number(input)
     input.length == 1 && (input =~ /[0-9]/) == 0 && input.to_i >= 0
   end
+
+  def contains_a_delimiter(input)
+    input.start_with?("//")
+  end
+
+  def sum_of_numbers(input)
+    if contains_a_delimiter(input)
+      delimiter = custom_delimiter(input)
+      numbers = extract_numbers_with_delimiter(input, delimiter)
+    else
+      numbers = extract_numbers_with_default_delimiters(input)
+    end
+    handle_negative_numbers(numbers)
+  end
+
+  def custom_delimiter(input)
+    input.split("//").last[0]
+  end
+
+  def extract_numbers_with_delimiter(input, delimiter)
+    input.split("\n").last.split(delimiter).map(&:to_i)
+  end
+
+  def extract_numbers_with_default_delimiters(input)
+    input.split(/[,\n]/).map(&:to_i)
+  end
+
+  def handle_negative_numbers(numbers)
+    negative_numbers = get_negative_numbers(numbers)
+    if negative_numbers.length == 1
+      return negative_numbers.first
+    elsif negative_numbers.any?
+      return "Negative numbers not allowed #{negative_numbers.join(',')}"
+    else
+      numbers.sum
+    end
+  end
+
+  def get_negative_numbers(numbers)
+    numbers.select { |n| n.negative? }
+  end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -10,4 +10,32 @@ RSpec.describe StringCalculator do
   it "returns the same number for a single digit string" do
     expect(string_calculator.add("1")).to eq(1)
   end
+
+  it "returns the sum of digits when two digits are given separated by comma" do
+    expect(string_calculator.add("1,5")).to eq(6)
+  end
+
+  it "returns the sum of digits when multiple digits are given separated by comma" do
+    expect(string_calculator.add("1,5,6,3")).to eq(15)
+  end
+
+  it "returns the sum of digits containing new lines between numbers in the string" do
+    expect(string_calculator.add("1\n2,3")).to eq(6)
+  end
+
+  it "returns 'Invalid Input' for a string containing new lines at the end" do
+    expect(string_calculator.add("1,\n")).to eq("Invalid Input")
+  end
+
+  it "returns the sum of digits when a delimiter is given in the string" do
+    expect(string_calculator.add("//;\n1;2")).to eq(3)
+  end
+  
+  it "returns 'Negative Numbers not allowed' for a string containing negative numbers" do
+    expect(string_calculator.add("1,-5, -3")).to eq("Negative numbers not allowed -5,-3")
+  end
+  
+  it "returns the single negative number when only one negative number is provided" do
+    expect(string_calculator.add("-7")).to eq(-7)
+  end
 end
