0a6c3ae6761f488daad9f972b7f12124683ddf8e
Divya <chdivya569@gmail.com>
2024-03-11 18:00:25 +0530

    for single digit string

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -5,4 +5,8 @@ class StringCalculator
     return "Invalid Input" if input.end_with?("\n")
     sum_of_numbers(input)
   end
+
+  def contains_a_single_number(input)
+    input.length == 1 && (input =~ /[0-9]/) == 0 && input.to_i >= 0
+  end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe StringCalculator do
   it "returns 0 for an empty string" do
     expect(string_calculator.add("")).to eq(0)
   end
+
+  it "returns the same number for a single digit string" do
+    expect(string_calculator.add("1")).to eq(1)
+  end
 end
